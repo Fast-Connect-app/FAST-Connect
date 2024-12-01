f69cb3a9efd99bf6f76b7fb5650214128a43cdff
Monarch789 <muhammadarhamali003@gmail.com>
2024-12-01 17:42:43 +0500

    User Auth bug fix

--- Backend/UserAuth/UserAuthentication.ts
+++ Backend/UserAuth/UserAuthentication.ts
@@ -12,12 +12,12 @@ export class UserAuthentication{
     //Singleton
     public static Instance:UserAuthentication;
 
-    static CreateInstance():void{
-        UserAuthentication.Instance = new UserAuthentication();
-    }
+    public GetInstance(){
+        if (UserAuthentication.Instance == null){
+            UserAuthentication.Instance = new UserAuthentication();
+        }
 
-    static HasInstance():boolean{
-        return UserAuthentication.Instance != null;
+        return UserAuthentication.Instance;
     }
 
     async CreateUser(_email:string, _password:string):Promise<string>{
