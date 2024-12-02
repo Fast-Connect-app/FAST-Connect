2b9a30988baa9e42c6011a90aa8fc9ca81388919
Monarch789 <muhammadarhamali003@gmail.com>
2024-12-02 13:20:14 +0500

    deleted the image and a tag function

--- Backend/ConvertBase64.ts
+++ Backend/ConvertBase64.ts
@@ -6,19 +6,3 @@ function ConvertToBase64(file) {
         reader.readAsDataURL(file); // Start reading the file as a base64 string
     });
 }
-
-function SetImageRef(imageRefId, data){
-    const avatar = document.getElementById(imageRefId); 
-    if (avatar) {
-        const img = avatar.querySelector('img'); 
-        if (img) {
-            img.src = data; 
-        } 
-    }
-}
-
-function SetDownloadLink(downloadLinkId, data){
-    const downloadLink = document.getElementById(downloadLinkId) as HTMLAnchorElement;
-    downloadLink.href = data; // Set the Base64 image data as the href
-    downloadLink.download = "downloaded_image.png"; // Set the file name
-}
