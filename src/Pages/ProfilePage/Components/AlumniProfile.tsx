64d010805259ac20f5f315b91fd6d2149b3bca85
Kooale325 <muhammad.bilal832017@gmail.com>
2024-12-03 20:16:22 +0500

    everything works for now

--- src/Pages/ProfilePage/Components/AlumniProfile.tsx
+++ src/Pages/ProfilePage/Components/AlumniProfile.tsx
@@ -2,7 +2,7 @@ import { Card, Box, Button, CardContent, TextField, Typography } from "@mui/mate
 import Grid from "@mui/material/Grid2";
 import { useState } from "react";
 import styles from "./Common.module.css"; // Import the CSS module
-import { AlumniProfile } from "../../../../Backend/Classes/AlumniProfile";
+import { Job, AlumniProfile } from "../../../../Backend/Classes/AlumniProfile";
 
 const AlumniProfileCard = (props: { alumniProfile: AlumniProfile; onSave: (updatedProfile: AlumniProfile) => Promise<void> }) => {
   const [editing, setEditing] = useState(false);
@@ -11,96 +11,23 @@ const AlumniProfileCard = (props: { alumniProfile: AlumniProfile; onSave: (updat
 
   const alumniProfile = props.alumniProfile;
   const onEditConfirm = async () => {
-    alumniProfile.jobHistory = editable[0];
-    alumniProfile.dateOfGraduation = editable[1];
+    alumniProfile.jobHistory = editable[0] as Job[];
+    alumniProfile.dateOfGraduation = editable[1] as Date;
     await props.onSave(alumniProfile);
   };
   return (
     <Grid size={{ xs: 12, md: 8 }}>
       <Card className={styles.contactCard}>
-        <CardContent>
-          <Box>
-            {/* ID specifies if it should be editable and if so what its position is in the editable string array */}
-            {[
-              { label: "Full Name", id: 0, value: userProfile.userName },
-              { label: "Email", id: -1, value: userProfile.email },
-              { label: "Gender", id: -2, value: userProfile.gender.toUpperCase() },
-              { label: "Roll No", id: 1, value: userProfile.rollNumber },
-              { label: "Bio", id: 2, value: userProfile.bio },
-            ].map((field, index) => (
-              <Box
-                key={index}
-                sx={{
-                  display: "flex",
-                  justifyContent: "space-between",
-                  alignItems: "center",
-                  mt: index === 0 ? 0 : 2,
-                }}
-              >
-                <Typography variant="h6" className={styles.contactHeading} sx={{ flex: "0 0 1" }}>
-                  {field.label}
-                </Typography>
-                {/* If its editing then its a textfield otherwise its a typography */}
-                {editing && field.id >= 0 ? (
-                  <TextField
-                    value={editable[field.id]}
-                    onChange={(e) => {
-                      const temp = [...editable];
-                      temp[field.id] = e.target.value;
-                      setEditable(temp);
-                    }}
-                  />
-                ) : (
-                  <Typography>{field.value}</Typography>
-                )}
-              </Box>
-            ))}
-            {!editing ? (
-              <Button
-                variant="contained"
-                color="secondary"
-                size="small"
-                sx={{ mt: 2 }}
-                disabled={!editingControlsEnabled}
-                onClick={() => {
-                  setEditing(true);
-                }}
-              >
-                Edit
-              </Button>
-            ) : (
-              <Box>
-                <Button
-                  variant="contained"
-                  color="secondary"
-                  size="small"
-                  sx={{ mt: 2 }}
-                  disabled={!editingControlsEnabled}
-                  onClick={async () => {
-                    setEditingControlsEnabled(false);
-                    await onEditConfirm();
-                    setEditingControlsEnabled(true);
-                    setEditing(false);
-                  }}
-                >
-                  OK
-                </Button>
-                <Button
-                  variant="contained"
-                  color="secondary"
-                  size="small"
-                  sx={{ mt: 2 }}
-                  disabled={!editingControlsEnabled}
-                  onClick={() => {
-                    setEditing(false);
-                  }}
-                >
-                  CANCEL
-                </Button>
-              </Box>
-            )}
-          </Box>
-        </CardContent>
+        <Box
+          sx={{
+            display: "flex",
+            justifyContent: "space-between",
+            alignItems: "center",
+          }}
+        >
+          <Typography>Date of Graduation</Typography>
+          <Typography>{new Date().toDateString()}</Typography>
+        </Box>
       </Card>
     </Grid>
   );
